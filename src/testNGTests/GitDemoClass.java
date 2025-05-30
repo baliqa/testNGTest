ee767367a4d11c7bb387a8fbc9747ca7622e72fe
belaiti <belaitiali.qa@gmail.com>
2025-05-30 14:05:09 -0400

    second commit

---
+++ src/testNGTests/GitDemoClass.java
@@ -0,0 +1,16 @@
+package testNGTests;
+
+import org.testng.annotations.Test;
+
+public class GitDemoClass {
+
+	@Test
+	public void demoCodes() {
+		
+		System.out.println("this is 1st code");
+		System.out.println("this is 2nd code");
+		System.out.println("this is 3rd code");
+		System.out.println("this is 4th code");
+		
+	}
+}
