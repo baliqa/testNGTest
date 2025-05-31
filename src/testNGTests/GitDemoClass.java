8d362a877aaa7f946b1a20135802e3d3f54ffdce
belaiti <belaitiali.qa@gmail.com>
2025-05-30 20:14:13 -0400

    first commit for develop branch

--- src/testNGTests/GitDemoClass.java
+++ src/testNGTests/GitDemoClass.java
@@ -19,4 +19,34 @@ public class GitDemoClass {
 		System.out.println("this is 10th code");
 		
 	}
+	@Test
+	public void demoCodes2() {
+		
+		System.out.println("this is 1st code");
+		System.out.println("this is 2nd code");
+		System.out.println("this is 3rd code");
+		System.out.println("this is 4th code");
+		System.out.println("this is 5rd code");
+		System.out.println("this is 6th code");
+		System.out.println("this is 7th code");
+		System.out.println("this is 8th code");
+		System.out.println("this is 9th code");
+		System.out.println("this is 10th code");
+		
+	}
+	@Test
+	public void demoCodes3() {
+		
+		System.out.println("this is 1st code");
+		System.out.println("this is 2nd code");
+		System.out.println("this is 3rd code");
+		System.out.println("this is 4th code");
+		System.out.println("this is 5rd code");
+		System.out.println("this is 6th code");
+		System.out.println("this is 7th code");
+		System.out.println("this is 8th code");
+		System.out.println("this is 9th code");
+		System.out.println("this is 10th code");
+		
+	}
 }
