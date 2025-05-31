ee15746ecfbae82d8086b85c91c03c3e5cfafd3b
belaiti <belaitiali.qa@gmail.com>
2025-05-30 21:27:05 -0400

    added some codes to master file that need to be merged to develope branch

--- src/testNGTests/GitDemoClass.java
+++ src/testNGTests/GitDemoClass.java
@@ -49,4 +49,50 @@ public class GitDemoClass {
 		System.out.println("this is 10th code");
 		
 	}
+	
+	@Test
+	public void demoCodes4() {
+		
+		System.out.println("this is 1st code");
+		System.out.println("this is 2nd code");
+		System.out.println("this is 3rd code");
+		System.out.println("this is 4th code");
+		System.out.println("this is 5rd code");
+		System.out.println("this is 6th code");
+		System.out.println("this is 7th code");
+		System.out.println("this is 8th code");
+		System.out.println("this is 9th code");
+		System.out.println("this is 10th code");
+		
+	}
+	@Test
+	public void demoCodes5() {
+		
+		System.out.println("this is 1st code");
+		System.out.println("this is 2nd code");
+		System.out.println("this is 3rd code");
+		System.out.println("this is 4th code");
+		System.out.println("this is 5rd code");
+		System.out.println("this is 6th code");
+		System.out.println("this is 7th code");
+		System.out.println("this is 8th code");
+		System.out.println("this is 9th code");
+		System.out.println("this is 10th code");
+		
+	}
+	@Test
+	public void demoCodes6() {
+		
+		System.out.println("this is 1st code");
+		System.out.println("this is 2nd code");
+		System.out.println("this is 3rd code");
+		System.out.println("this is 4th code");
+		System.out.println("this is 5rd code");
+		System.out.println("this is 6th code");
+		System.out.println("this is 7th code");
+		System.out.println("this is 8th code");
+		System.out.println("this is 9th code");
+		System.out.println("this is 10th code");
+		
+	}
 }
